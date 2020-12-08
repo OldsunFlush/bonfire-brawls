61368bdcacaccbb6d10b50e9e211cbca51d90278
Pierre Prézelin <pierre@helloasso.com>
2020-12-08 15:50:51 +0100

    Updated packages

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,45 +1,54 @@
-export default {
-  // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
-  ssr: false,
 
-  // Global page headers (https://go.nuxtjs.dev/config-head)
+export default {
+  mode: 'universal',
+  /*
+  ** Headers of the page
+  */
   head: {
-    title: 'BonfireBrawls',
+    title: 'Bonfire Brawls',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: 'A game about bonfires and brawls' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      {
+        rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'
+      }
     ]
   },
-
-  // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-  ],
-
-  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: ['~/assets/styles/styles.scss'],
+  /*
+  ** Plugins to load before mounting the App
+  */
   plugins: [
+    { src: '~/plugins/persisted-state.js', ssr: false }
   ],
-
-  // Auto import components (https://go.nuxtjs.dev/config-components)
-  components: true,
-
-  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  /*
+  ** Nuxt.js dev-modules
+  */
   buildModules: [
   ],
-
-  // Modules (https://go.nuxtjs.dev/config-modules)
+  /*
+  ** Nuxt.js modules
+  */
   modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
   ],
-
-  // Axios module configuration (https://go.nuxtjs.dev/config-axios)
-  axios: {},
-
-  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  /*
+  ** Build configuration
+  */
   build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend (config, ctx) {
+    }
   }
 }
